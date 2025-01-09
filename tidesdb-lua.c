95256cbbb1b1326605f711a1127dadf45074c1cf
Alex Gaetano Padula <me@alexpadula.com>
2025-01-09 00:25:56 -0500

    Added clang format, added code_formatter.sh, formatted code, added header comments.

--- tidesdb-lua.c
+++ tidesdb-lua.c
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (C) Evgeny Kornev
+ * Copyright (C) TidesDB
  *
  * Original Author: Evgeny Kornev
  *
@@ -16,7 +16,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
